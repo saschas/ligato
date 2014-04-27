cc0e0279d921564e2cc7e5b751ef4acd45e3910a
saschasigl <sascha.sigl@googlemail.com>
2014-04-27 15:24:18 +0200

    select next coin
    Signed-off-by: saschasigl <sascha.sigl@googlemail.com>

--- source/script.js
+++ source/script.js
@@ -1,17 +1,46 @@
 $(document).ready(function(){
 
-	var countDupli = function(myArray,$selector){
-		var counts = [];
-		for (i = 0; i < myArray.length; i++) {
-			counts[i] = 0;
-			for (j = 0; j < myArray.length; j++) {
-				if (myArray[i] == myArray[j]) {
-					counts[i]++;
-				}
-			}
+	var cleanArray = function(myArray){
+		var out = [];
+		var myBigObject = myArray[0];
+
+		for (var i in myBigObject) {
+		    if (myBigObject.hasOwnProperty(i)) {
+		        out.push(myBigObject[i]);
+		    }
 		}
-		if($selector != undefined){
-			return counts[$selector];	
+		console.log('out:' + out);
+		return out;
+	}
+
+	var checkCoinsCount = function($array,$selector){
+		var toGo;
+		array_elements = $array;
+		array_elements.sort();
+		var current = null;
+		var cnt = 0;
+		for (var i = 0; i < array_elements.length; i++) {
+		    if (array_elements[i] != current) {
+		        if (cnt > 0) {
+		        	if(current == $selector){
+		        		//How many moves you have
+		        		toGo = cnt;
+		        		return toGo;
+		        	}
+		        }
+		        current = array_elements[i];
+		        cnt = 1;
+		    } else {
+		        cnt++;
+		    }
+		}
+		if (cnt > 0) {
+				if(current == $selector){
+		        	//How many moves you have
+		        	toGo = cnt;
+		        	return toGo;
+		        }
+		    //console.log(current + ' comes --> ' + cnt + ' times');
 		}
 	}
 
@@ -36,7 +65,7 @@ $(document).ready(function(){
 			moves:20,
 			currentCoins:0,
 			row:[],
-			coins: []
+			coins: [{}]
 		}
 		$actions = {};
 		$('#menu p').text($player.moves);
@@ -45,6 +74,13 @@ $(document).ready(function(){
 		console.clear();
 		states($game);
 	}//end of setup
+
+	var coinElement = function($actions,$player){
+		var element = document.createElement('div');
+			element.setAttribute('class','coin ' + $player.id);
+			element.player = $player.id;
+			$('.row:nth-child('+$actions.row+') .field:nth-child('+$actions.field+')').append(element);
+	}
 
 	var coin = function($action,$player){
 			$condition = true;
@@ -70,7 +106,7 @@ $(document).ready(function(){
 			if($condition === true){
 				$player.currentCoins++;	
 				$player.row.push($actions.row);
-				$player.coins[$actions.row] = $actions.field;
+				$player.coins[0][$actions.row-1] = $actions.field;
 				$player.moves -= $actions.field;//noch x schritte
 				$('#menu p').text($player.moves + ' / 20');
 
@@ -96,10 +132,7 @@ $(document).ready(function(){
 				});
 
 			//Creates Coin
-			var element = document.createElement('div');
-				element.setAttribute('class','coin ' + $player.id);
-				element.player = $player.id;
-				$actions.parent.append(element);
+			coinElement($actions,$player);			
 			}
 	}
 
@@ -107,7 +140,7 @@ $(document).ready(function(){
 			$actions.parent = el;
 			$actions.row = parseFloat(el.parent().index()+1);
 			$actions.field = parseFloat(el.index()+1);
-
+			console.log('$actions.field: ' + $actions.field);
 			if($game.whileGame == true){
 
 			}
@@ -119,41 +152,73 @@ $(document).ready(function(){
 		$('.row').removeClass('used');
 	}
 
-	var jump = function($count){
-		console.log($count);
-		var possibleFieldPlus = $actions.field + $count;
-		var possibleFieldMinus = $actions.field - $count;
+	var choices = function($moves){
+		//console.log($count);
+		var possibleFieldPlus = $actions.field + $moves;
+		var possibleFieldMinus = $actions.field - $moves;
 
-		console.log(possibleFieldPlus, possibleFieldMinus)
+		//console.log(possibleFieldPlus, possibleFieldMinus)
 		if(possibleFieldPlus<=10){
-			$('.row:nth-child('+$actions.row+') .field:nth-child('+possibleFieldPlus+')').addClass('choice');
-			console.log('minus Choice: ' + possibleFieldPlus);
+			var $selectorPlus = $('.row:nth-child('+$actions.row+') .field:nth-child('+possibleFieldPlus+')')
+			$selectorPlus.addClass('choice-up');
+			//console.log('minus Choice: ' + possibleFieldPlus);
 		}
 		if(possibleFieldMinus>0){
-			$('.row:nth-child('+$actions.row+') .field:nth-child('+possibleFieldMinus+')').addClass('choice');
-			console.log('minus Choice: ' + possibleFieldMinus);
+			var $selectorMinus = $('.row:nth-child('+$actions.row+') .field:nth-child('+possibleFieldMinus+')')
+			$selectorMinus.addClass('choice-down');
+			//console.log('minus Choice: ' + possibleFieldMinus);
 		}
-		$('.field.choice').click(function(){
-
+		$('.field.choice-up').click(function(){
+			$player.row[$actions.row] = possibleFieldPlus;
+		});
+		$('.field.choice-down').click(function(){
+			$player.row[$actions.row] = possibleFieldMinus;
 		});
 	}
 
 	var states = function($game){
-		$('.field.nope').click(function(){
+		$('.field').click(function(){
 			if($game.beforeGame == true && $game.whileGame == false ){
 				console.log('beforeGame');
 				coin(action($(this)),$player);
 			}
 			if($game.whileGame == true){
-				console.log('whileGame');
-				action($(this));
-				var $count = countDupli($player.coins,$actions.field);
-				jump($count);
+				var $checkforCoin = $(this).has('.coin').length;
+				var $checkforPossibleCoin = $(this).is('.choice-up, .choice-down');
+				console.log($checkforCoin);
+
+				//mark possible Moves
+				if($checkforCoin>0){
+					console.log('whileGame');
+					action($(this));
+					console.log('$player.coins: '+$player.coins);
+					$player.row = cleanArray($player.coins);
+					var $movesToGo = checkCoinsCount($player.row,$actions.field);
+					choices($movesToGo);
+				}
+
+				//click on Choice
+				if($checkforPossibleCoin){
+					var choice = $(this).index()+1;//$('.row:nth-child('+$actions.row+') .field:nth-child('+$player.row[$actions.row]+')')
+					var removeCoinSelector = $player.row[$actions.row];
+					$('.row:nth-child('+$actions.row+') .field:nth-child('+removeCoinSelector+')').remove('coin')
+					$player.row[$actions.row] = choice;
+
+					console.log($actions,$player);
+					coinElement($actions,$player);
+
+					//$('.row:nth-child('+$actions.row+') .field:nth-child('+removeCoinSelector+')');
+					console.log('du bist dabei auf feld '+choice+' zu springen');
+				}
+				else{return false;}		
 			}
 			else{
 				console.log('dount')
 			}
-		})
+
+		});
+			
+		
 	}
 	
 	
